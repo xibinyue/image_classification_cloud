67e705554e37ffc6ac60a15b1df3032384ccf0cf
xibin.yue <xibin.yue@moji.com>
2017-06-05 11:54:57 +0800

    init

--- image_classification.py
+++ image_classification.py
@@ -7,29 +7,30 @@ This code is based on https://github.com/BVLC/caffe/tree/master/examples
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-caffe_root = '/Users/seanlu/caffe/'
+
+caffe_root = '~/xibin.yue/caffe/'
 sys.path.insert(0, caffe_root + 'python')
 import caffe
 import urllib
 
-
 import os
+
 
 def initialize_caffe():
     if os.path.isfile(caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'):
         print 'CaffeNet found.'
     else:
         print 'Downloading pre-trained CaffeNet model...'
-        #!../scripts/download_model_binary.py ../models/bvlc_reference_caffenet
+        # !../scripts/download_model_binary.py ../models/bvlc_reference_caffenet
 
     caffe.set_mode_cpu()
 
     model_def = caffe_root + 'models/bvlc_reference_caffenet/deploy.prototxt'
     model_weights = caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'
 
-    net = caffe.Net(model_def,      # defines the structure of the model
+    net = caffe.Net(model_def,  # defines the structure of the model
                     model_weights,  # contains the trained weights
-                    caffe.TEST)     # use test mode (e.g., don't perform dropout)
+                    caffe.TEST)  # use test mode (e.g., don't perform dropout)
 
     # load the mean ImageNet image (as distributed with Caffe) for subtraction
     mu = np.load(caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy')
@@ -37,26 +38,26 @@ def initialize_caffe():
     print 'mean-subtracted values:', zip('BGR', mu)
 
     transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
-    transformer.set_transpose('data', (2,0,1))  # move image channels to outermost dimension
-    transformer.set_mean('data', mu)            # subtract the dataset-mean value in each channel
-    transformer.set_raw_scale('data', 255)      # rescale from [0, 1] to [0, 255]
-    transformer.set_channel_swap('data', (2,1,0))  # swap channels from RGB to BGR
+    transformer.set_transpose('data', (2, 0, 1))  # move image channels to outermost dimension
+    transformer.set_mean('data', mu)  # subtract the dataset-mean value in each channel
+    transformer.set_raw_scale('data', 255)  # rescale from [0, 1] to [0, 255]
+    transformer.set_channel_swap('data', (2, 1, 0))  # swap channels from RGB to BGR
     # load ImageNet labels
     labels_file = caffe_root + 'data/ilsvrc12/synset_words.txt'
     if not os.path.exists(labels_file):
         os.system('../data/ilsvrc12/get_ilsvrc_aux.sh')
-        
+
     labels = np.loadtxt(labels_file, str, delimiter='\t')
     return net, labels, transformer
 
-if __name__ == "__main__":
 
-    net,labels,transformer = initialize_caffe()
+if __name__ == "__main__":
+    net, labels, transformer = initialize_caffe()
     # download an image
-    my_image_url = "https://upload.wikimedia.org/wikipedia/commons/4/4c/Push_van_cat.jpg"  
+    my_image_url = "https://upload.wikimedia.org/wikipedia/commons/4/4c/Push_van_cat.jpg"
     # for example:
-    #my_image_url = "https://upload.wikimedia.org/wikipedia/commons/b/be/Orang_Utan%2C_Semenggok_Forest_Reserve%2C_Sarawak%2C_Borneo%2C_Malaysia.JPG"
-    #os.system('wget -O'+ ' image.jpg'+ my_image_url)
+    # my_image_url = "https://upload.wikimedia.org/wikipedia/commons/b/be/Orang_Utan%2C_Semenggok_Forest_Reserve%2C_Sarawak%2C_Borneo%2C_Malaysia.JPG"
+    # os.system('wget -O'+ ' image.jpg'+ my_image_url)
     urllib.urlretrieve(my_image_url, filename="image.png")
 
     # transform it and copy it into the net
@@ -72,7 +73,5 @@ if __name__ == "__main__":
     # sort top five predictions from softmax output
     top_inds = output_prob.argsort()[::-1][:5]
 
-
     print 'probabilities and labels:'
     print (zip(output_prob[top_inds], labels[top_inds]))
-
--- myserver.py
+++ myserver.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory,session
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session
 from werkzeug import secure_filename
 
 import image_classification
@@ -8,18 +8,17 @@ from image_classification import initialize_caffe
 import urllib
 
 import sys
-caffe_root = '/Users/seanlu/caffe/'
+
+caffe_root = '~/xibin.yue/caffe/'
 sys.path.insert(0, caffe_root + 'python')
 import caffe
 
-
-
 tag_data = {}
 
-net,labels,transformer = initialize_caffe()
+net, labels, transformer = initialize_caffe()
 
 # Initialize the Flask application
-app = Flask(__name__,static_url_path = "", static_folder = "uploads")
+app = Flask(__name__, static_url_path="", static_folder="uploads")
 
 # This is the path to the upload directory
 app.config['UPLOAD_FOLDER'] = 'uploads/'
@@ -37,20 +36,19 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 
-@app.route('/',methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-
     print (tag_data)
 
     if request.method == 'POST':
         if request.form.get("subject", None) == "Upload an image":
             upload_an_image = True
             return render_template('index.html', upload_an_image=upload_an_image)
-        
+
         elif request.form.get("subject", None) == "Search images based on tags":
             search_an_image = True
             return render_template('index.html', search_an_image=search_an_image)
-        
+
         elif request.form.get("subject", None) == "start_search":
             search_an_image = True
             start_search = True
@@ -64,26 +62,24 @@ def index():
                 print (i_file)
                 print (i_tags)
                 if (searched_tag in str(i_tags)):
-                
                     s_filenames.append(str(i_file))
 
             if (len(s_filenames) == 0):
                 messages = "No images found!"
                 return render_template('index.html', search_an_image=search_an_image, messages=messages)
             else:
-                #for i in s_filenames:
+                # for i in s_filenames:
                 #    print (i)
                 return render_template('index.html', search_an_image=search_an_image, s_filenames=s_filenames)
-
 
     filename = session.get('filename')
     if (filename != None):
 
-        #print type(filename)
-        #filename is string
+        # print type(filename)
+        # filename is string
         # transform it and copy it into the net
         print (os.getcwd())
-        path_to_file = './uploads/'+filename
+        path_to_file = './uploads/' + filename
         image = caffe.io.load_image(path_to_file)
         net.blobs['data'].data[...] = transformer.preprocess('data', image)
 
@@ -96,10 +92,9 @@ def index():
         # sort top five predictions from softmax output
         top_inds = output_prob.argsort()[::-1][:5]
 
-
-        #print 'probabilities and labels:'
+        # print 'probabilities and labels:'
         p_labels = []
-        for i in range (0,5):
+        for i in range(0, 5):
             tokens = labels[top_inds][i].split(' ')
             p_label = ''
             for j in range(1, len(tokens)):
@@ -107,14 +102,14 @@ def index():
             p_labels.append(p_label)
 
         p_and_tags = zip(output_prob[top_inds], p_labels)
-        #print (zip(output_prob[top_inds], p_labels))
-        #print (zip(output_prob[top_inds], labels[top_inds])[0])
+        # print (zip(output_prob[top_inds], p_labels))
+        # print (zip(output_prob[top_inds], labels[top_inds])[0])
 
         tag_data[filename] = p_labels
         print (tag_data)
 
-        return render_template('index.html', filename = filename, p_and_tags=p_and_tags)
-    
+        return render_template('index.html', filename=filename, p_and_tags=p_and_tags)
+
     return render_template('index.html')
 
 
@@ -136,13 +131,14 @@ def upload():
         print (filename)
     return redirect(url_for('index'))
 
+
 # This route is expecting a parameter containing the name
 # of a file. Then it will locate that file on the upload
 # directory and show it on the browser, so if the user uploads
 # an image, that image is going to be show after the upload
 
-#@app.route('/uploads/<filename>')
-#def uploaded_file(filename):
+# @app.route('/uploads/<filename>')
+# def uploaded_file(filename):
 #    print (filename)
 #    return send_from_directory(app.config['UPLOAD_FOLDER'],
 #                               filename)
@@ -157,9 +153,9 @@ if __name__ == "__main__":
     @click.argument('HOST', default='0.0.0.0')
     @click.argument('PORT', default=8111, type=int)
     def run(debug, threaded, host, port):
-
         HOST, PORT = host, port
         print "running on %s:%d" % (HOST, PORT)
         app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
-    
+
+
     run()
